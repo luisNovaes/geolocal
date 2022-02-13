61911daaa2c6fa64a01c96329aa623e8b0cd9955
luisNovaes <luismagnovaes@gmail.com>
2022-02-13 17:42:13 +0000

    first commit

--- src/datasources/base.datasource.ts
+++ src/datasources/base.datasource.ts
@@ -12,10 +12,6 @@ const config = {
   database: 'postgres',
 };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class BaseDataSource extends juggler.DataSource
   implements LifeCycleObserver {
